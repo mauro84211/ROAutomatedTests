5cde749e5bf1b9ffcb71b4b490f4118a5ad44280
mauro84211 <mauro211@gmail.com>
2020-10-29 16:37:37 -0400

    con BaseTest
    trabajando con la la BasesTets, y creando la HomePage para continuar.

--- src/test/java/Steps.java
+++ src/test/java/Steps.java
@@ -1,43 +1,37 @@
+import base.BaseTest;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import pages.HomePage;
+import pages.LoginPage;
 
-public class Steps {
-    private WebDriver driver;
-    private ChromeOptions getChromeOptions(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("disable-infobars");
-        options.setAcceptInsecureCerts(true);
-        //options.setHeadless(true); /*Para que no se levante el navegador durante las pruebas*/
-        return options;
-    }
-
+public class Steps extends BaseTest {
+    LoginPage loginPage;
+    HomePage homePage;
 
     @Given("I am in the login page of the RO System")
     public void iAmInTheLoginPageOfTheROSystem() {
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver(getChromeOptions());
-        driver.get("https://rosws.generalsoftwareinc.net:8004/login");
-
+        setUp();
+        loginPage = new LoginPage(driver);
     }
 
     @When("I enter valid credentials")
     public void iEnterValidCredentials() {
-        /*Hay que buscar aqui ahora como locarlizar lejor los campos usuario y contraseñá*/
-        driver.findElement(By.id("username")).sendKeys("rojupiter");
-        driver.findElement(By.id("password")).sendKeys("rojupiter");
-        driver.findElement(By.id("company")).sendKeys("rojupiter");
-        driver.findElement(By.xpath("//button[@class='bp3-button bp3-active bp3-fill bp3-large sc-ePZHVD hbcjPd']") ).click();
 
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("username")));
+        /*Hay que buscar aqui ahora como locarlizar lejor los campos usuario y contraseñá*/
+        loginPage.setUsername("rojupiter");
+        loginPage.setPassword("rojupiter");
+        loginPage.setCompany("rojupiter");
+        homePage = loginPage.clickLoginButton();
     }
 
     @Then("I should be taken to the Home page")
     public void iShouldBeTakenToTheHomePage() {
-        Assert.assertEquals(driver.getTitle(),"Welltrax - Bulk Transportation Management", "Not Login");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("search")));
+        Assert.assertEquals(homePage.getUrl(), "https://rosws.generalsoftwareinc.net:8004/setup/locations/pickup", "Not Login");
     }
 }
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,28 +3,32 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
-    private WebDriver driver;
+    protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeMethod
-    public void goHome() {
-        driver.get("https://the-internet.herokuapp.com/");
+    public void goStart() {
+        driver.get("https://rosws.generalsoftwareinc.net:8004/login");
+        wait = new WebDriverWait(driver, 5);
     }
 
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new ChromeDriver();
-        goHome();
+        driver = new ChromeDriver(getChromeOptions());
+        goStart();
     }
 
     private ChromeOptions getChromeOptions() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("disable-infobars");
+        options.setAcceptInsecureCerts(true);
         //options.setHeadless(true); /*Para que no se levante el navegador durante las pruebas*/
         return options;
     }
